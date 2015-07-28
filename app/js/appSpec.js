34e90bd1e6e11cafae580327d4f0e6ddcc27db7e
angelarrr <kwon.angela@gmail.com>
2015-07-28 11:16:00 -0400

    finish testing of services

--- app/js/appSpec.js
+++ app/js/appSpec.js
@@ -29,14 +29,14 @@ describe("appFactory", function(){
 		$httpBackend.verifyNoOutstandingRequest();
 	});
 
-	describe('getCountry', function(){
+	describe('getCountries', function(){
 
 		it('should successfully request countries data',
-			inject(function($httpBackend, getCountry){
-				var response = 'an array of countries'
+			inject(function($httpBackend, getCountries){
+				var response = 'an array of countries';
 				$httpBackend.expectGET('http://api.geonames.org/countryInfoJSON?username=angelarrr').respond(response);
 
-				getCountry().then(function(data){
+				getCountries().then(function(data){
 					$rootScope.digest();
 					$httpBackend.flush();
 					expect(data).toBe('an array of countries');
@@ -44,16 +44,62 @@ describe("appFactory", function(){
 			}));
 	});
 
+	// use country code AR for testing
+
+	describe('getCountry', function(){
+
+		it('should successfully request detail on specific country',
+			inject(function($httpBackend, getCountry){
+				var response = 'details of a country';
+				$httpBackend.expectGET('http://api.geonames.org/countryInfoJSON?&country=AR&username=angelarrr').respond(response);
+
+				getCountry().then(function(data){
+					$rootScope.digest();
+					$httpBackend.flush();
+					expect(data).toBe('details of a country');
+				});
+			}));
+	});
+
+	describe('getCapital', function(){
+
+		it('should successfully return capital details',
+			inject(function($httpBackend, getCapital){
+				var response = 'capital details';
+				$httpBackend.expectGET('http://api.geonames.org/searchJSON?&country=AR&username=angelarrr').respond(response);
+
+				getCapital().then(function(data){
+					$rootScope.digest();
+					$httpBackend.flush();
+					expect(data).toBe('capital details');
+				});
+			}));
+	});
+
 	describe('getNeighbors', function(){
 		it('should successfully request neighbors data',
 			inject(function($httpBackend, getNeighbors){
-				var response = 'an array of neighbors'
+				var response = 'an array of neighbors';
 				$httpBackend.expectGET('http://api.geonames.org/neighboursJSON?&country=AR&username=angelarrr').respond(response);
 
 				getNeighbors().then(function(data){
 					$rootScope.digest();
 					$httpBackend.flush();
 					expect(data).toBe('an array of neighbors');
+				});
+			}));
+	});
+
+	describe('getTimeZone', function(){
+		it('should successfully request timezone information',
+			inject(function($httpBackend, getTimeZone){
+				var response = 'timezone ID';
+				$httpBackend.expectGET('http://api.geonames.org/timezoneJSON?&lat=-34.61315&lng=-58.37723&username=angelarrr').respond(response);
+
+				getTimeZone().then(function(data){
+					$rootScope.digest();
+					$httpBackend.flush();
+					expect(data).toBe('timezone ID');
 				});
 			}));
 	});
